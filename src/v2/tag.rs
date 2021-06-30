0706fbb247cb5f315d678f1cba2d58c5fe22d0e7
Andy-Python-Programmer <andypythonappdeveloper@gmail.com>
2021-06-30 19:07:01 +1000

    Add the memory map tag
    * Add the memory map tag and its child structures
    * Add documentation to those structures
    
    Signed By: Anhad Singh

--- src/v2/tag.rs
+++ src/v2/tag.rs
@@ -1,3 +1,5 @@
+use core::marker::PhantomData;
+
 #[repr(C, packed)]
 pub struct StivaleTagHeader {
     pub identifier: u64,
@@ -84,4 +86,90 @@ pub struct StivaleRsdpTag {
     pub header: StivaleTagHeader,
     /// Pointer to the ACPI RSDP structure.
     pub rsdp: u64,
+}
+
+/// The type of a memory map entry. The entries are guaranteed to be sorted by base address,
+/// lowest to highest.
+///
+/// ## Alignment
+/// Usable and bootloader reclaimable entries are guaranteed to be 4096 byte aligned for both
+/// base and length. Usable and bootloader reclaimable entries are **guaranteed** not to overlap with
+/// any other entry.
+#[repr(u32)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum StivaleMemoryMapEntryType {
+    /// Usable memory.
+    Usable = 1,
+    /// Memory reserved by the system.
+    Reserved = 2,
+    /// ACPI memory that can be reclaimed.
+    AcpiReclaimable = 3,
+    /// ACPI memory that cannot be reclaimed.
+    AcpiNvs = 4,
+    /// Memory marked as defective (bad RAM).
+    BadMemory = 5,
+    /// Memory used by the bootloader that can be reclaimed after it's not being used anymore.
+    BootloaderReclaimable = 0x1000,
+    /// Memory containing the kernel and any modules.
+    Kernel = 0x1001,
+    /// Memory containing the framebuffer.
+    Framebuffer = 0x1002,
+}
+
+#[repr(C, packed)]
+#[derive(Debug, Copy, Clone)]
+pub struct StivaleMemoryMapEntry {
+    /// Physical address of base of the memory section.
+    pub base: u64,
+    /// Length of this memory section.
+    pub length: u64,
+    /// The type of this memory map entry.
+    pub entry_type: StivaleMemoryMapEntryType,
+
+    padding: u32,
+}
+
+#[repr(C, packed)]
+pub struct StivaleMemoryMapTag {
+    pub header: StivaleTagHeader,
+    /// Total length of the memory map entries.
+    pub entries_len: u64,
+    /// Slice of the memory map entries.
+    pub entry_array: [StivaleMemoryMapEntry],
+}
+
+impl StivaleMemoryMapTag {
+    /// Returns an iterator over all the memory regions.
+    pub fn iter(&self) -> StivaleMemoryMapIter {
+        StivaleMemoryMapIter {
+            sref: self,
+            current: 0x00,
+            phantom: PhantomData::default(),
+        }
+    }
+}
+
+/// Iterator over all the memory memory regions provided by the stivale bootloader.
+#[derive(Clone)]
+pub struct StivaleMemoryMapIter<'a> {
+    /// A reference to the stivale memory map tag.
+    sref: &'a StivaleMemoryMapTag,
+    /// The index of the memory map entry that we are about to index.
+    current: u64,
+    phantom: PhantomData<&'a StivaleMemoryMapEntry>,
+}
+
+impl<'a> Iterator for StivaleMemoryMapIter<'a> {
+    type Item = &'a StivaleMemoryMapEntry;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current < self.sref.entries_len {
+            let entry = &self.sref.entry_array[self.current as usize];
+            self.current += 1;
+
+            Some(entry)
+        } else {
+            None
+        }
+    }
 }
