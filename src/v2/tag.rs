1dff08b318c29e37a179a0f595a3a0cc77cde103
Andy-Python-Programmer <andypythonappdeveloper@gmail.com>
2021-07-01 11:54:40 +1000

    Finish adding all of the stivalev2 tags
    * Finish adding all of the stivale2 tags and I still have to add them in
    the main stivale structure
    
    Signed By: Anhad Singh

--- src/v2/tag.rs
+++ src/v2/tag.rs
@@ -1,5 +1,7 @@
 use core::marker::PhantomData;
 
+use super::header::StivaleSmpHeaderTagFlags;
+
 #[repr(C, packed)]
 pub struct StivaleTagHeader {
     pub identifier: u64,
@@ -134,12 +136,12 @@ pub struct StivaleMemoryMapTag {
     pub header: StivaleTagHeader,
     /// Total length of the memory map entries.
     pub entries_len: u64,
-    /// Slice of the memory map entries.
+    /// Pointer to the memory map entries.
     pub entry_array: [StivaleMemoryMapEntry; 0],
 }
 
 impl StivaleMemoryMapTag {
-    /// Return's the slice of memory map entries.
+    /// Return's memory map entries pointer as a rust slice.
     pub fn as_slice(&self) -> &[StivaleMemoryMapEntry] {
         unsafe { core::slice::from_raw_parts(self.entry_array.as_ptr(), self.entries_len as usize) }
     }
@@ -154,7 +156,7 @@ impl StivaleMemoryMapTag {
     }
 }
 
-/// Iterator over all the memory memory regions provided by the stivale bootloader.
+/// Iterator over all the memory regions provided by the stivale bootloader.
 #[derive(Clone)]
 pub struct StivaleMemoryMapIter<'a> {
     /// A reference to the stivale memory map tag.
@@ -229,4 +231,228 @@ pub struct StivaleKernelSlideTag {
     pub header: StivaleTagHeader,
     /// The kernel slide. See structure-level documentation for more information.
     pub kernel_slide: u64,
+}
+
+/// This tag is used to get the kernel the command line string that was passed to it by
+/// the bootloader.
+#[repr(C, packed)]
+pub struct StivaleCommandLineTag {
+    pub header: StivaleTagHeader,
+    /// Pointer to a null-terminated cmdline.
+    pub command_line: u64,
+}
+
+/// This tag is used to get the EDID information as acquired by the firmware.
+#[repr(C, packed)]
+pub struct StivaleEdidInfoTag {
+    pub header: StivaleTagHeader,
+    /// Length of the EDID information array.
+    pub edid_len: u64,
+    /// Pointer to the EDID information array.
+    pub info_array: [u8; 0],
+}
+
+impl StivaleEdidInfoTag {
+    /// Return's the EDID information pointer as a rust slice.
+    pub fn as_slice(&self) -> &[u8] {
+        unsafe { core::slice::from_raw_parts(self.info_array.as_ptr(), self.edid_len as usize) }
+    }
+}
+
+/// This tag exists if MTRR write-combining for the framebuffer was requested and successfully enabled. See
+/// the documentation of [crate::v2::header::StivaleMtrrHeaderTag] for more information.
+///
+/// ## Legacy
+/// This tag is deprecated and considered legacy. Use is discouraged and it may not be supported on newer bootloaders.
+#[deprecated(
+    note = "This tag is deprecated and considered legacy. Use is discouraged and it may not be supported on newer bootloaders."
+)]
+#[repr(C, packed)]
+pub struct StivaleMtrrTag {
+    pub header: StivaleTagHeader,
+}
+
+/// Structure representing a module, containing the information of a module that
+/// the bootloader loaded alongside the kernel.
+#[repr(C, packed)]
+pub struct StivaleModule {
+    /// Address where this module has been loaded.
+    pub start: u64,
+    /// End address of this module.
+    pub end: u64,
+    /// ASCII 0-terminated string passed to the module as specified in
+    /// the config file.
+    pub string: [u8; 128],
+}
+
+impl StivaleModule {
+    /// Returns the size of this module.
+    #[inline]
+    pub fn size(&self) -> u64 {
+        self.end - self.start
+    }
+
+    /// Returns the ASCII 0-terminated string passed to the module as specified in the config file
+    /// as a rust string.
+    #[inline]
+    pub fn as_str(&self) -> &str {
+        super::utils::string_from_slice(&self.string)
+    }
+}
+
+/// Iterator over all the modules that were loaded.
+#[derive(Clone)]
+pub struct StivaleModuleIter<'a> {
+    /// A reference to the stivale module tag.
+    sref: &'a StivaleModuleTag,
+    /// The index of the module entry that we are about to index.
+    current: u64,
+    phantom: PhantomData<&'a StivaleModule>,
+}
+
+impl<'a> Iterator for StivaleModuleIter<'a> {
+    type Item = &'a StivaleModule;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current < self.sref.module_len {
+            let entry = &self.sref.as_slice()[self.current as usize];
+            self.current += 1;
+
+            Some(entry)
+        } else {
+            None
+        }
+    }
+}
+
+/// This tag is used to get the modules that the bootloader loaded alongside the kernel, if any.
+#[repr(C, packed)]
+pub struct StivaleModuleTag {
+    pub header: StivaleTagHeader,
+    /// Length of the modules array.
+    pub module_len: u64,
+    /// Pointer to the modules array.
+    pub modules_array: [StivaleModule; 0],
+}
+
+impl StivaleModuleTag {
+    /// Returns an iterator over all the modules that were loaded.
+    pub fn iter(&self) -> StivaleModuleIter {
+        StivaleModuleIter {
+            sref: self,
+            current: 0,
+            phantom: PhantomData::default(),
+        }
+    }
+
+    /// Return's the modules array pointer as a rust slice.
+    pub fn as_slice(&self) -> &[StivaleModule] {
+        unsafe {
+            core::slice::from_raw_parts(self.modules_array.as_ptr(), self.module_len as usize)
+        }
+    }
+}
+
+/// This tag is used to get the location of the SMBIOS entry points in memory.
+#[repr(C, packed)]
+pub struct StivaleSmbiosTag {
+    pub header: StivaleTagHeader,
+    /// Stivale specification says that the flags in this tag are for future use
+    /// and currently should be unused and **must** be 0.
+    pub flags: u64,
+    /// 32-bit SMBIOS entry point address. Set to 0 if unavailable.
+    pub smbios_entry_32: u64,
+    /// 64-bit SMBIOS entry point address. Set to 0 if unavailable.
+    pub smbios_entry_64: u64,
+}
+
+/// SMP imformation structure.
+#[repr(C, packed)]
+pub struct StivaleSmpInfo {
+    /// ACPI Processor UID as specified by MADT.
+    pub acpi_processor_uid: u32,
+    /// LAPIC ID as specified by MADT.
+    pub lapic_id: u32,
+    /// The stack that will be loaded in ESP/RSP once the goto_address field is loaded.
+    /// This MUST point to a valid stack of at least 256 bytes in size, and 16-byte aligned.
+    /// target_stack is an unused field for the struct describing the BSP.
+    pub target_stack: u64,
+    /// This field is polled by the started APs until the kernel on another CPU performs an
+    /// atomic write to this field. When that happens, bootloader code will load up ESP/RSP with the stack
+    /// value as specified in target_stack. It will then proceed to load a pointer to this very structure into
+    /// either register RDI for 64-bit or on the stack for 32-bit, then, goto_address is called (a bogus return
+    /// address is pushed onto the stack) and execution is handed off.
+    /// The CPU state will be the same as describedin "kernel entry machine state", with the exception
+    /// of ESP/RSP and RDI/stack arg being set up as above. goto_address is an unused field for the
+    /// struct describing the BSP.
+    pub goto_address: u64,
+    /// This field is polled by the started APs until the kernel on another CPU performs an
+    /// atomic write to this field. When that happens, bootloader code will
+    /// load up ESP/RSP with the stack value as specified in target_stack.
+    /// It will then proceed to load a pointer to this very structure into either register
+    /// RDI for 64-bit or on the stack for 32-bit, then, goto_address is called (a bogus return
+    /// address is pushed onto the stack) and execution is handed off.
+    ///
+    /// The CPU state will be the same as described in "kernel entry machine state", with the exception
+    /// of ESP/RSP and RDI/stack arg being set up as above. goto_address is an unused field for the
+    /// struct describing the BSP.
+    pub extra: u64,
+}
+
+#[repr(C, packed)]
+pub struct StivaleSmpTag {
+    pub header: StivaleTagHeader,
+    pub flags: StivaleSmpHeaderTagFlags,
+    /// LAPIC ID of the BSP (bootstrap processor).
+    pub bsp_lapic_id: u32,
+    /// Stivale specification says that this field is reserved for future use.
+    pub unused: u32,
+    /// The total number of logical CPUs (including BSP).
+    pub cpu_count: u64,
+    /// Pointer to the SMP info array (including BSP).
+    pub smp_info_array: [StivaleSmpInfo; 0],
+}
+
+impl StivaleSmpTag {
+    /// Return's the SMP info array pointer as a rust slice.
+    pub fn as_slice(&self) -> &[StivaleSmpInfo] {
+        unsafe {
+            core::slice::from_raw_parts(self.smp_info_array.as_ptr(), self.cpu_count as usize)
+        }
+    }
+}
+
+/// This tag reports that the kernel has been booted via PXE, and reports the server ip that
+/// it was booted from.
+#[repr(C, packed)]
+pub struct StivalePxeInfoTag {
+    pub header: StivaleTagHeader,
+    /// Server IP in network byte order.
+    pub server_ip: u32,
+}
+
+/// This tag reports that there is a memory mapped UART port and its address.
+#[repr(C, packed)]
+pub struct StivaleUartTag {
+    pub header: StivaleTagHeader,
+    /// The address of the UART port.
+    pub address: u64,
+}
+
+/// This tag describes a device tree blob for the platform.
+#[repr(C, packed)]
+pub struct StivaleDeviceTreeTag {
+    pub header: StivaleTagHeader,
+    /// The address of the device tree blob.
+    pub address: u64,
+    /// The size of the device tree blob.
+    pub size: u64,
+}
+
+/// This tag describes the high physical memory location.
+#[repr(C, packed)]
+pub struct StivaleVMap {
+    pub header: StivaleTagHeader,
+    /// VMAP_HIGH, where the physical memory is mapped in the higher half.
+    pub address: u64,
 }
