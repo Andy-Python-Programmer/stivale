3d83002a39013d59d11280241e45c7dd57882506
Andy-Python-Programmer <andypythonappdeveloper@gmail.com>
2021-07-08 14:03:12 +1000

    Finish adding support for v1 structs
    * Finish adding support for adding stivale v1 structure and header.
    
    Signed By: Anhad Singh

--- src/v1/mod.rs
+++ src/v1/mod.rs
@@ -2,6 +2,10 @@
 //! to be a simple to implement protocol which provides the kernel with most of the features one may
 //! need in a modern x86_64 context (although 32-bit x86 is also supported).
 
+mod utils;
+
+use core::marker::PhantomData;
+
 union StivaleHeaderEntryPoint {
     func: extern "C" fn(&'static StivaleStruct) -> !,
     zero: u16,
@@ -67,7 +71,224 @@ impl StivaleHeader {
     }
 }
 
+/// Structure representing a module, containing the information of a module that
+/// the bootloader loaded alongside the kernel.
+#[repr(C, packed)]
+pub struct StivaleModule {
+    /// Address where this module has been loaded.
+    pub start: u64,
+    /// End address of this module.
+    pub end: u64,
+    /// ASCII 0-terminated string passed to the module as specified in
+    /// the config file.
+    pub string: [u8; 128],
+}
+
+impl StivaleModule {
+    /// Returns the size of this module.
+    #[inline]
+    pub fn size(&self) -> u64 {
+        self.end - self.start
+    }
+
+    /// Returns the ASCII 0-terminated string passed to the module as specified in the config file
+    /// as a rust string.
+    #[inline]
+    pub fn as_str(&self) -> &str {
+        self::utils::string_from_slice(&self.string)
+    }
+}
+
+/// Iterator over all the modules that were loaded.
+#[derive(Clone)]
+pub struct StivaleModuleIter<'a> {
+    /// A reference to the stivale structure.
+    sref: &'a StivaleStruct,
+    /// The index of the module entry that we are about to index.
+    current: u64,
+    phantom: PhantomData<&'a StivaleModule>,
+}
+
+impl<'a> Iterator for StivaleModuleIter<'a> {
+    type Item = &'a StivaleModule;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current < self.sref.module_len {
+            let entry = &self.sref.modules_as_slice()[self.current as usize];
+            self.current += 1;
+
+            Some(entry)
+        } else {
+            None
+        }
+    }
+}
+
+/// The type of a memory map entry. The entries are guaranteed to be sorted by base address,
+/// lowest to highest.
+///
+/// ## Alignment
+/// Usable and bootloader reclaimable entries are guaranteed to be 4096 byte aligned for both
+/// base and length. Usable and bootloader reclaimable entries are **guaranteed** not to overlap with
+/// any other entry.
+#[repr(u32)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum StivaleMemoryMapEntryType {
+    /// Usable memory.
+    Usable = 1,
+    /// Memory reserved by the system.
+    Reserved = 2,
+    /// ACPI memory that can be reclaimed.
+    AcpiReclaimable = 3,
+    /// ACPI memory that cannot be reclaimed.
+    AcpiNvs = 4,
+    /// Memory marked as defective (bad RAM).
+    BadMemory = 5,
+    /// Memory used by the bootloader that can be reclaimed after it's not being used anymore.
+    BootloaderReclaimable = 0x1000,
+    /// Memory containing the kernel and any modules.
+    Kernel = 0x1001,
+    /// Memory containing the framebuffer.
+    Framebuffer = 0x1002,
+}
+
+#[repr(C, packed)]
+#[derive(Debug, Copy, Clone)]
+pub struct StivaleMemoryMapEntry {
+    /// Physical address of base of the memory section.
+    pub base: u64,
+    /// Length of this memory section.
+    pub length: u64,
+    /// The type of this memory map entry.
+    pub entry_type: StivaleMemoryMapEntryType,
+
+    padding: u32,
+}
+
+impl StivaleMemoryMapEntry {
+    /// Returns the end address of this memory region.
+    #[inline]
+    pub fn end_address(&self) -> u64 {
+        self.base + self.length
+    }
+
+    /// Returns the entry type of this memory region. External function is required
+    /// as reference the entry_type packed field is not aligned.
+    #[inline]
+    pub fn entry_type(&self) -> StivaleMemoryMapEntryType {
+        self.entry_type
+    }
+}
+
+/// Iterator over all the memory regions provided by the stivale bootloader.
+#[derive(Clone)]
+pub struct StivaleMemoryMapIter<'a> {
+    /// A reference to the stivale structure.
+    sref: &'a StivaleStruct,
+    /// The index of the memory map entry that we are about to index.
+    current: u64,
+    phantom: PhantomData<&'a StivaleMemoryMapEntry>,
+}
+
+impl<'a> Iterator for StivaleMemoryMapIter<'a> {
+    type Item = &'a StivaleMemoryMapEntry;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current < self.sref.memory_map_len {
+            let entry = &self.sref.memory_map_as_slice()[self.current as usize];
+            self.current += 1;
+
+            Some(entry)
+        } else {
+            None
+        }
+    }
+}
+
 #[repr(C, packed)]
 pub struct StivaleStruct {
-    // TODO:
+    /// Address of the null-terminated command line.
+    pub command_line: u64,
+    /// Pointer to the memory map array.
+    pub memory_map_array: [StivaleMemoryMapEntry; 0],
+    /// Length of the memory map entries.
+    pub memory_map_len: u64,
+
+    /// Address of the framebuffer if avaliable else its set to zero.
+    pub framebuffer_addr: u64,
+    /// The framebuffer pitch in bytes.
+    pub framebuffer_pitch: u16,
+    /// Width of the framebuffer in pixels.
+    pub framebuffer_width: u16,
+    /// Height of the framebuffer in pixels.
+    pub framebuffer_height: u16,
+    /// The framebuffer bits per pixels.
+    pub framebuffer_bpp: u16,
+
+    /// Address of the RSDP ACPI structure.
+    pub rsdp_adddres: u64,
+
+    /// The length of modules that the stivale bootloader loaded according to the
+    /// config.
+    pub module_len: u64,
+    /// Pointer to the modules array.
+    pub modules: [StivaleModule; 0],
+
+    /// UNIX epoch at boot, which is read from system RTC.
+    pub unix_epoch: u64,
+    pub flags: u64,
+
+    /// Size of the red mask in RGB.
+    pub red_mask_size: u8,
+    /// Shift of the red mask in RGB.
+    pub red_mask_shift: u8,
+    /// Size of the green mask in RGB.
+    pub green_mask_size: u8,
+    /// Shift of the green mask in RGB.
+    pub green_mask_shift: u8,
+    /// Size of the blue mask in RGB.
+    pub blue_mask_size: u8,
+    /// Shift of the blue mask in RGB.
+    pub blue_mask_shift: u8,
+    reserved: u8,
+
+    /// 32-bit SMBIOS entry point address. Set to 0 if unavailable.
+    pub smbios_entry_32: u64,
+    /// 64-bit SMBIOS entry point address. Set to 0 if unavailable.
+    pub smbios_entry_64: u64,
+}
+
+impl StivaleStruct {
+    /// Return's the modules array pointer as a rust slice.
+    pub fn modules_as_slice(&self) -> &[StivaleModule] {
+        unsafe { core::slice::from_raw_parts(self.modules.as_ptr(), self.module_len as usize) }
+    }
+
+    /// Returns an iterator over all the modules that were loaded.
+    pub fn modules_iter(&self) -> StivaleModuleIter {
+        StivaleModuleIter {
+            sref: self,
+            current: 0,
+            phantom: PhantomData::default(),
+        }
+    }
+
+    /// Return's memory map entries pointer as a rust slice.
+    pub fn memory_map_as_slice(&self) -> &[StivaleMemoryMapEntry] {
+        unsafe {
+            core::slice::from_raw_parts(
+                self.memory_map_array.as_ptr(),
+                self.memory_map_len as usize,
+            )
+        }
+    }
+
+    /// Returns an iterator over all the memory regions.
+    pub fn memory_map_iter(&self) -> StivaleMemoryMapIter {
+        StivaleMemoryMapIter {
+            sref: self,
+            current: 0x00,
+            phantom: PhantomData::default(),
+        }
+    }
 }
